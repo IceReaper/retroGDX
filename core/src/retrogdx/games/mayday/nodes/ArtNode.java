40a6484ce31c706328471fd6237ec5b12a6ed66c
Andre Mohren <andremohren@gmail.com>
2018-03-12 15:44:27 +0100

    Mayday: Conflict Earth Art format.

--- core/src/retrogdx/games/mayday/nodes/ArtNode.java
+++ core/src/retrogdx/games/mayday/nodes/ArtNode.java
@@ -1,29 +1,53 @@
 package retrogdx.games.mayday.nodes;
 
 import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.Tree;
-import com.badlogic.gdx.utils.Array;
 import retrogdx.games.mayday.readers.Art;
-import retrogdx.ui.AssetFolderNode;
+import retrogdx.ui.AssetFileNode;
+import retrogdx.ui.previews.ImageSetPreview;
 import retrogdx.utils.SmartByteBuffer;
 
-import java.util.Map;
+import java.nio.ByteOrder;
 
-public class ArtNode extends AssetFolderNode {
+public class ArtNode extends AssetFileNode {
     public ArtNode(Table previewArea, FileHandle file) {
         super(previewArea, file);
     }
 
-    protected Array<Tree.Node> populate() {
+    protected void showPreview() {
         Art art = new Art(SmartByteBuffer.wrap(this.file.readBytes()));
 
-        Array<Tree.Node> nodes = new Array<>();
+        Sprite[] sprites = new Sprite[art.frames.length];
 
-        for (Map.Entry<String, SmartByteBuffer> file : art.getFiles().entrySet()) {
-            System.out.println(file.getKey());
+        // TODO case insensitive.
+        SmartByteBuffer paletteBuffer = SmartByteBuffer.wrap(this.file.sibling("DSpiel.pal").readBytes());
+        paletteBuffer.order(ByteOrder.LITTLE_ENDIAN);
+        paletteBuffer.position(0x36);
+
+        int[] palette = new int[256];
+
+        for (int i = 0; i < palette.length - 1; i++) {
+            palette[i] = (paletteBuffer.readInt() << 8) | 0xff;
         }
 
-        return nodes;
+        for (int i = 0; i < art.frames.length; i++) {
+            Art.ArtFrame frame = art.frames[i];
+            Pixmap pixmap = new Pixmap(frame.width, frame.height, Pixmap.Format.RGBA8888);
+
+            for (int y = 0; y < frame.height; y++) {
+                for (int x = 0; x < frame.width; x++) {
+                    int index = frame.pixels[x + y * frame.width] & 0xff;
+                    pixmap.drawPixel(x, y, palette[index]);
+                }
+            }
+
+            sprites[i] = new Sprite(new Texture(pixmap));
+            sprites[i].setOrigin(frame.width / 2, frame.height / 2);
+        }
+
+        this.previewArea.add(new ImageSetPreview(sprites));
     }
 }
--- core/src/retrogdx/games/mayday/readers/Art.java
+++ core/src/retrogdx/games/mayday/readers/Art.java
@@ -3,24 +3,80 @@ package retrogdx.games.mayday.readers;
 import retrogdx.utils.SmartByteBuffer;
 
 import java.nio.ByteOrder;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 public class Art {
-    private SmartByteBuffer buffer;
-
-    public Art(SmartByteBuffer buffer) {
-        this.buffer = buffer;
+    public class ArtFrame {
+        public byte[] pixels;
+        public int width;
+        public int height;
     }
 
-    public Map<String, SmartByteBuffer> getFiles() {
-        this.buffer.order(ByteOrder.LITTLE_ENDIAN);
-        this.buffer.position(0);
+    public ArtFrame[] frames;
 
-        Map<String, SmartByteBuffer> files = new LinkedHashMap<>();
+    public Art(SmartByteBuffer buffer) {
+        buffer.order(ByteOrder.LITTLE_ENDIAN);
+        buffer.position(0);
 
-        // TODO what the hell. this file seems to have no index?! containts images 8bpp indexed, uncompressed.
+        // TODO try to find information about frame sizes, so we can get rid of this hardcoded  list.
 
-        return files;
+        byte[] unk1 = buffer.readBytes(4 * 10);
+        byte[] unk2 = buffer.readBytes(4 * 46);
+        byte[] unk3 = buffer.readBytes(14);
+
+        this.frames = new ArtFrame[7334];
+
+        for (int i = 0; i < this.frames.length; i++) {
+            ArtFrame frame = new ArtFrame();
+
+            if (i < 2884) {
+                frame.width = 64;
+                frame.height = 32;
+            } else if (i < 2884 + 960) {
+                frame.width = 32;
+                frame.height = 32;
+            } else if (i < 2884 + 960 + 2848) {
+                frame.width = 64;
+                frame.height = 64;
+            } else if (i < 2884 + 960 + 2848 + 113) {
+                frame.width = 128;
+                frame.height = 128;
+            } else if (i < 2884 + 960 + 2848 + 113 + 281) {
+                frame.width = 64;
+                frame.height = 64;
+            } else if (i < 2884 + 960 + 2848 + 113 + 281 + 64) {
+                frame.width = 32;
+                frame.height = 32;
+            } else if (i < 2884 + 960 + 2848 + 113 + 281 + 64 + 1) {
+                frame.width = 164;
+                frame.height = 25;
+            } else if (i < 2884 + 960 + 2848 + 113 + 281 + 64 + 1 + 58) {
+                frame.width = 90;
+                frame.height = 64;
+            } else if (i < 2884 + 960 + 2848 + 113 + 281 + 64 + 1 + 58 + 1) {
+                frame.width = 164;
+                frame.height = 498;
+            } else if (i < 2884 + 960 + 2848 + 113 + 281 + 64 + 1 + 58 + 1 + 50) {
+                frame.width = 154;
+                frame.height = 105;
+            } else if (i < 2884 + 960 + 2848 + 113 + 281 + 64 + 1 + 58 + 1 + 50 + 16) {
+                frame.width = 20;
+                frame.height = 20;
+            } else if (i < 2884 + 960 + 2848 + 113 + 281 + 64 + 1 + 58 + 1 + 50 + 16 + 1) {
+                frame.width = 164;
+                frame.height = 189;
+            } else if (i < 2884 + 960 + 2848 + 113 + 281 + 64 + 1 + 58 + 1 + 50 + 16 + 1 + 48) {
+                frame.width = 64;
+                frame.height = 64;
+            } else if (i < 2884 + 960 + 2848 + 113 + 281 + 64 + 1 + 58 + 1 + 50 + 16 + 1 + 48 + 1) {
+                frame.width = 320;
+                frame.height = 286;
+            } else {
+                frame.width = 32;
+                frame.height = 32;
+            }
+
+            frame.pixels = buffer.readBytes(frame.width * frame.height);
+            this.frames[i] = frame;
+        }
     }
 }
